6ea931268bfb57b971ba71fac6a2b261ece57f2e
Jakub Podolski <60115575+bebsimax@users.noreply.github.com>
2022-04-09 16:25:06 +0200

    Noun class added. Database structure in progress. Repetitions and synonyms to go.

---
+++ Nordek/Models/Noun.cs
@@ -0,0 +1,14 @@
+﻿namespace Nordek.Models;
+
+public class Noun
+{
+    /* Entall - singular, Flertall - plural
+     Ubestemt - unspecified, Bestemt - specified */
+    
+    private string EntallU { get; set; }
+    private string EntallB { get; set; }
+    private string FlertallU { get; set; }
+    private string FlertallB { get; set; }
+    
+    
+}
